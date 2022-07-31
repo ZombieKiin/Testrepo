5b305ce690a46902b2f3034342a08dd5d4d9780e
Thomas <tommylebaronjr@gmail.com>
2022-07-31 03:58:51 -0600

    updated main

---
+++ src/main/java/com/example/niotesting/Main.java
@@ -0,0 +1,39 @@
+package com.example.niotesting;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class Main {
+    public static void main(String[] args) {
+        try(FileOutputStream binFile = new FileOutputStream("data.dat");
+        FileChannel binChannel = binFile.getChannel()) {
+        
+        byte[] outputBytes = "Hello World!".getBytes();
+        ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+        int numBytes = binChannel.write(buffer);
+        System.out.println("numBytes written was: " + numBytes);
+
+        ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+        intBuffer.putInt(245);
+        intBuffer.flip();
+        numBytes = binChannel.write(intBuffer);
+        System.out.println("numBytes written was: " + numBytes);
+        
+        intBuffer.flip();
+        intBuffer.putInt(-98765);
+        intBuffer.flip();
+        numBytes = binChannel.write(intBuffer);
+        System.out.println("numBytes written was: " + numBytes);
+
+        RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+        byte[] b = new byte[outputBytes.length];
+        ra.read(b);
+        System.out.println(new String(b));
+        } catch(IOException e) {
+          e.printStackTrace();
+        }
+        }
+    }
