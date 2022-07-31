41ae935b580b8d3e0ae80ea332b78277dc69acd4
Thomas <tommylebaronjr@gmail.com>
2022-07-30 19:25:45 -0600

    Changing HelloController

--- src/main/java/com/example/niotesting/HelloController.java
+++ src/main/java/com/example/niotesting/HelloController.java
@@ -6,4 +6,5 @@ import javafx.scene.control.Label;
 public class HelloController {
     //This is where I will write nonsense to see if Git will pick it up
     //here is test number 2 where this will not be recorded
+    //Here is the 3rd commit and one that should add HelloController
 }
